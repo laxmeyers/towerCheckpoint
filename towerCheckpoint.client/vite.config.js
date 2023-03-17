030a80ecd78ccc07753e7fbfb20c54caa65d74fd
Andrew Meyers <laxmeyers@gmail.com>
2023-03-17 14:30:21 -0600

    live serve

--- towerCheckpoint.client/vite.config.js
+++ towerCheckpoint.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../towerCheckpoint/client',
     sourcemap: false
   },
   server: {
